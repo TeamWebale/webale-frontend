3dd5eb45ab36b662b5c40ea211f845a04adaf6ce
Landfolks <landfolks@ymail.com>
2026-02-17 19:19:11 +0100

    Revert API URL to onrender.com

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://api.webale.net/api';
+const API_URL = 'https://webale-api.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
